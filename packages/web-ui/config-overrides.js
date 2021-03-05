f8cda663ee3f3cdde37008f193ec080c1da9d650
Ross Leitch <professional.balbatross@gmail.com>
2021-03-05 16:29:49 +1300

    Fs

--- packages/web-ui/config-overrides.js
+++ packages/web-ui/config-overrides.js
@@ -1,6 +1,14 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
 
 module.exports = function override(config, env){
+
+    if(env == 'production'){
+        config = rewireWebpackBundleAnalyzer(config, env, {
+            analyzerMode: 'static',
+            reportFilename: 'report.html'
+        })
+    }
 
     config.resolve.plugins.pop();
 
@@ -11,7 +19,6 @@ module.exports = function override(config, env){
 
     tsRule.include = undefined;
     tsRule.exclude = /node_modules/;
-
 
     return config;
 }
