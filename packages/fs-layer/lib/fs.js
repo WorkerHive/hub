ad2cad1a93c3ae43a13ff281d8575c1bcd3f31ff
Ross Leitch <professional.balbatross@gmail.com>
2021-03-05 15:16:26 +1300

    Fs

---
+++ packages/fs-layer/lib/fs.js
@@ -0,0 +1,21 @@
+const IPFS = require('ipfs');
+const {P2PStack} = require('./p2p-stack')
+
+export const FSNode = async (config, repo, key) => {
+    const node = await IPFS.create({
+        repo: repo,
+        libp2p: P2PStack(key),
+        config: {
+            Bootstrap: config.Bootstrap || [],
+            Addresses: {
+                Swarm: config.Swarm || [],
+            },
+            Discovery: {
+                webRTCStar: {Enabled: true},
+                MDNS: {Enabled: true}
+            }
+        },
+        relay: {enabled: true, hop: {enabled: true}}
+    })
+    return {node, libp2p: node.libp2p}
+}
